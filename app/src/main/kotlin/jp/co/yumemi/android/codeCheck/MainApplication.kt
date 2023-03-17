efa0c94906cb9eda9b8b103ba7b687bcfeb63fe1
hirotask <hirotaskhashi@gmail.com>
2023-03-17 15:46:00 +0900

    add Timber library

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/MainApplication.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/MainApplication.kt
@@ -2,7 +2,13 @@ package jp.co.yumemi.android.codeCheck
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 }
